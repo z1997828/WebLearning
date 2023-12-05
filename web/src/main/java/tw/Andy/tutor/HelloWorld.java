036b7a782e045d13611f504a2f8d6cbb623711de
Andy <z1997828@gmail.com>
2023-12-05 16:30:04 +0800

    1205Web

---
+++ web/src/main/java/tw/Andy/tutor/HelloWorld.java
@@ -0,0 +1,24 @@
+package tw.Andy.tutor;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	
+    public HelloWorld() {
+    
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+
+}
